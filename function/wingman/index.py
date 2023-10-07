39dcc59a7210810260793afce2aae460478cda80
miztch <miztch.me@gmail.com>
2023-10-08 05:42:19 +0900

    feat: new ongoing events will also be notified

--- function/wingman/index.py
+++ function/wingman/index.py
@@ -15,18 +15,18 @@ def lambda_handler(event, context) -> None:
     # get events from DynamoDB table
     saved_events = database.get_saved_events()
 
-    # update DynamoDB table with "upcoming" events
-    database.put_events(vlr_events["upcoming"])
+    # update DynamoDB table with events "upcoming" or "ongoing"
+    database.put_events(vlr_events["upcoming"] + vlr_events["ongoing"])
 
-    # delete events "started" or "completed" if in DynamoDB table
+    # delete "completed" events if in DynamoDB table
     events_delete: list = []
-    for vlr_event in vlr_events["ongoing"] + vlr_events["completed"]:
+    for vlr_event in vlr_events["completed"]:
         if vlr_event["id"] in [saved_event["id"] for saved_event in saved_events]:
             events_delete.append(vlr_event)
     database.delete_events(events_delete)
 
-    # notify to channel if new event
-    for vlr_event in vlr_events["upcoming"]:
+    # notify to channel if new uncompleted event
+    for vlr_event in vlr_events["upcoming"] + vlr_events["ongoing"]:
         if vlr_event["id"] not in [saved_event["id"] for saved_event in saved_events]:
             try:
                 notify.send(vlr_event)
